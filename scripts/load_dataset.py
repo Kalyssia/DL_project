a0a5634beece6934f909f35ece45cee8d68bcde4
Aurélien Juillé <aurelien.houdusse.juille@gmail.com>
2025-05-12 03:53:37 +0200

    Project setup with script to load the dataset

---
+++ scripts/load_dataset.py
@@ -0,0 +1,24 @@
+import os
+import urllib.request
+import numpy as np
+from PIL import Image
+
+CLASSES = ["cat", "dog", "car", "tree", "bicycle", "airplane", "clock", "apple", "face", "house"]
+URL_BASE = "https://storage.googleapis.com/quickdraw_dataset/full/numpy_bitmap/"
+OUT_DIR = "data"
+MAX_IMAGES = 1000
+
+os.makedirs(OUT_DIR, exist_ok=True)
+
+for cls in CLASSES:
+    url = URL_BASE + cls.replace(" ", "%20") + ".npy"
+    npy_path = os.path.join(OUT_DIR, cls + ".npy")
+    urllib.request.urlretrieve(url, npy_path)
+    data = np.load(npy_path)
+    data = data.reshape(-1, 28, 28).astype(np.uint8)
+    class_dir = os.path.join(OUT_DIR, cls)
+    os.makedirs(class_dir, exist_ok=True)
+    for i in range(MAX_IMAGES):
+        img = Image.fromarray(data[i])
+        img.save(os.path.join(class_dir, f"{i}.png"))
+    os.remove(npy_path)
